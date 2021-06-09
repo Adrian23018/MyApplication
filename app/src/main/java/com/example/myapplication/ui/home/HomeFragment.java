cf9407f2d3b01a3779d05b4a7383e0e977c666c4
Adrian23018 <adrian.cardenas@correounivalle.edu.co>
2021-06-09 11:31:59 -0500

    Creacion LOGIN

--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -6,34 +6,86 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    Button BtnNew,btnEstadistica;
+
+    private EditText email, pass;
+    private Button boton;
+
+    private String email1="";
+    private String password="";
+
+    private FirebaseAuth mAugth;
+    private DatabaseReference mDatabase;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        View r = inflater.inflate(R.layout.fragment_home, container, false);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseApp.initializeApp(null);
 
-       // final TextView textView = root.findViewById(R.id.text_home);
+        email = r.findViewById(R.id.ediEmail);
+        pass = r.findViewById(R.id.ediPassword);
+        boton= r.findViewById(R.id.btnIniciar);
 
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        boton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-         //       textView.setText(s);
+            public void onClick(View view) {
+                email1= email.getText().toString();
+                password=pass.getText().toString();
+
+                if(!email1.isEmpty() && !password.isEmpty()){
+                    loginUser();
+                }else{
+                    Toast.makeText(getActivity().getApplicationContext(),"CAMPOS VACIOS ", Toast.LENGTH_SHORT).show();
+                }
+
+
             }
         });
-        return root;
+
+        return r;
+    }
+
+
+    private void loginUser(){
+
+       // FirebaseApp.initializeApp();
+        mAugth= FirebaseAuth.getInstance();
+        mAugth.signInWithEmailAndPassword(email1,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    //startActivity(new Intent(Logearse.this, Home.class));
+                    //finish();
+                    Toast.makeText(getActivity().getApplicationContext(), "SE HA INICIADO SESIÓN CORRECTAMENTE ", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getActivity().getApplicationContext(), "NO SE HA INICIADO SESIÓN COMPRUEBE LOS DATOS", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
 
