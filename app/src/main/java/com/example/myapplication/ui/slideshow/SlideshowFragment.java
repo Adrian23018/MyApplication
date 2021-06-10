afc6b4aa17481e9a2bd3e14c7e0ef51cb7f88fb5
Adrian23018 <adrian.cardenas@correounivalle.edu.co>
2021-06-10 17:38:25 -0500

    Creacion de tabla en la BD

--- app/src/main/java/com/example/myapplication/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/myapplication/ui/slideshow/SlideshowFragment.java
@@ -1,24 +1,40 @@
 package com.example.myapplication.ui.slideshow;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.example.myapplication.R;
+import com.example.myapplication.RegistrarUsuario;
 import com.example.myapplication.conexion.Conexion;
+import com.example.myapplication.conexion.ConexionTiempo;
+import com.example.myapplication.ui.home.HomeFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SlideshowFragment extends Fragment {
 
@@ -27,6 +43,10 @@ public class SlideshowFragment extends Fragment {
     ArrayList<String> lista;
     ArrayAdapter adaptador;
     EditText horaI,horaF;
+    Button guardar;
+    FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
+
     Date d=new Date();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -45,11 +65,43 @@ public class SlideshowFragment extends Fragment {
         String horaString2 = ho2.format(d);
         horaF.setText(horaString2);
 
+        guardar=root.findViewById(R.id.btnGuardar2);
+
+
        combo=root.findViewById(R.id.spinner);
        cargarDatos();
 
+
+       guardar.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               GuardarDatos();
+           }
+       });
+
         return root;
     }
+
+    public void GuardarDatos() {
+        String inicio = horaI.getText().toString();
+        String finals = horaF.getText().toString();
+        String spinner = combo.getSelectedItem().toString();
+
+        ConexionTiempo conexion = new ConexionTiempo(getActivity().getApplicationContext(), "TIEMPO", null, 1);
+        SQLiteDatabase db = conexion.getWritableDatabase();
+        if (db != null) {
+            System.out.println("Entro");
+            ContentValues registroNuevo = new ContentValues();
+            registroNuevo.put("horaInicial", inicio);
+            registroNuevo.put("horaFinal", finals);
+            registroNuevo.put("actividad", spinner);
+
+            db.insert("tiempo", null, registroNuevo);
+            Toast.makeText(getActivity().getApplicationContext(), "Datos Almacenados", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 
     public void cargarDatos(){
         Conexion conexion=new Conexion(getActivity().getApplicationContext(),"ACTIVIDAD2",null,1);
