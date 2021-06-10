44ecd4f5a4b0343cd0d8b0439492b79689c87707
Adrian23018 <adrian.cardenas@correounivalle.edu.co>
2021-06-10 10:14:50 -0500

    AGREGAR LOS TIEMPOS

--- app/src/main/java/com/example/myapplication/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/example/myapplication/ui/slideshow/SlideshowFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -15,7 +16,9 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.myapplication.R;
 import com.example.myapplication.conexion.Conexion;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class SlideshowFragment extends Fragment {
 
@@ -23,12 +26,24 @@ public class SlideshowFragment extends Fragment {
     Spinner combo;
     ArrayList<String> lista;
     ArrayAdapter adaptador;
+    EditText horaI,horaF;
+    Date d=new Date();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
             ViewGroup container, Bundle savedInstanceState) {
         slideshowViewModel =
                 ViewModelProviders.of(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+        horaI=root.findViewById(R.id.ediHoraIni);
+        SimpleDateFormat ho=new SimpleDateFormat("h:mm a");
+        String horaString = ho.format(d);
+        horaI.setText(horaString);
+
+        horaF=root.findViewById(R.id.ediHoraFin);
+        SimpleDateFormat ho2=new SimpleDateFormat("h:mm a");
+        String horaString2 = ho2.format(d);
+        horaF.setText(horaString2);
 
        combo=root.findViewById(R.id.spinner);
        cargarDatos();
